db58d5fcc2a7c338b395d9f67d08570131dc8690
AntezanaValentin <valentinescabj@gmail.com>
2022-11-02 16:59:53 -0300

    lestacopia

--- client/src/Form.js
+++ client/src/Form.js
@@ -32,21 +32,56 @@ const Form = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        onChange={handleChange}
-        value={text}
-        name="text"
-        type="text"
-        placeholder="Tu Mensaje"
-      />
-      <input
-        onChange={handleChange1}
-        value={img}
-        name="image"
-        type="text"
-        placeholder="Tu imagen"
-      />
-    <input className="btn-submit" type="submit" value="Enviar" />
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+        </div>
+        <div class="carousel-inner text-bg-">
+          <div class="carousel-item">
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+            <div class="container">
+              <div class="carousel-caption text-start text-bg-dark">
+                <h1>deberia estar arriba</h1>
+                <p className = "text-center text-bg-dark fs-3">Matematicas</p>
+                <p><a class="btn btn-lg btn-primary" href="#">Jugar</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item active">
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Preguntados la35</h1>
+                <p>Para jugar y elegir el tema de las preguntas o preguntar pase la pagina</p>
+
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+            <div class="container">
+              <div class="carousel-caption text-end">
+                <h1>One more for good measure.</h1>
+                <p className="text-center fs-3">Ajedrez</p>
+                <p><a class="btn btn-lg btn-dark" href="#">Jugar</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </form>
   );
 }
